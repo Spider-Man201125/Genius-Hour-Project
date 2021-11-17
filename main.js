4037ad30b104c7ff140f812d90e3bfddeacf25f6
Spider-Man201125 <86503283+Spider-Man201125@users.noreply.github.com>
2021-11-17 07:38:37 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function clickMcDonalds() {
+     window.location="McDonalds_Menu.html";
+}
+function clickChick_Fil_A() {
+     window.location="Chick-Fil-A.html";
+}
+function Back() {
+     window.location="restaurant.html";
+}
+function Diabetes() 
+{ window.alert("You are a diabetic patient; you are allowed to eat 1800 to 2000 calories and 100 to 120 grams of carbohydrates per day."); }
